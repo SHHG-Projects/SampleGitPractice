7e1d141bd8a71221ff4e5968309fdbcd7b81d14d
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-18 23:41:36 +0900

    Initial Commit

---
+++ SampleGitPractice/SampleGitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGitPracticeApp.swift
+//  SampleGitPractice
+//
+//  Created by 渡邊魁優 on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct SampleGitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
